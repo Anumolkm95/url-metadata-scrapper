e75d9870a22579007134f5675327df42ee69c2f7
Anumol K M <anumol.km@teamta.in>
2022-03-14 15:55:17 +0530

    post api and function added

---
+++ function/handler.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const cheerio = require('cheerio');
+const request = require('request');
+
+var getResponseObject = (body, code) => {
+  return  {
+    statusCode : code,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(body)
+  };
+}
+
+module.exports.urlMetadataScrapper = async (event, context) => {
+  let input = JSON.parse(event.body);
+  if (input.url) {
+    const requestOpts = {
+      url: input.url
+    };
+  
+    let promise = new Promise((resolve, reject) => {
+      request.get(requestOpts, function (err, response, body) {
+        if (err || !response) {
+          return resolve("FAILED");
+        }
+        if (response.statusCode && response.statusCode !== 200) {
+          return resolve("FAILED");
+        }
+        if (response.statusCode && response.statusCode === 200) {
+          let url = input.url;
+          if (response.request.uri.href) url = response.request.uri.href;
+          return resolve({url, body});
+        }
+      });
+    });
+    try {
+      let data = await promise;
+      if (data === "FAILED") {
+        console.log(data)
+        return getResponseObject({message: "Can't retrieve data!"}, 404);
+      }
+      const $ = cheerio.load(data.body);
+      const $metaTags = $('meta');
+      var extracted = {};
+      $metaTags.each(function (index, el) {
+        if ($(this).attr('content')) {
+          if ($(this).attr('name')) {
+            extracted[$(this).attr('name')] = $(this).attr('content');
+          }
+          if ($(this).attr('property')) {
+            extracted[$(this).attr('property')] = $(this).attr('content');
+          }
+          if ($(this).attr('itemprop')) {
+            extracted[$(this).attr('itemprop')] = $(this).attr('content');
+          }
+        }
+      });
+      return getResponseObject({data: extracted}, 200);
+    } catch (err) {
+      console.log(err);
+      return getResponseObject({message: "Something went wrong!"}, 500);
+    }
+  } else {
+    return getResponseObject({message: "Bad Request!"}, 400);
+  }
+};
---
+++ function/test/handler.spec.js
@@ -0,0 +1,49 @@
+const {urlMetadataScrapper} = require("../handler");
+const chai = require("chai");
+const sinonChai = require("sinon-chai");
+const request = require('request');
+const Sinon = require("sinon");
+const expect = chai.expect;
+// chai.use(sinonChai);
+
+describe("Url metadata scrapper", () => {
+    it("should return the metadata of the given url", (done) => {
+        // const getStub = Sinon.stub(request, 'get').resolves({url: "", body: ``})
+        urlMetadataScrapper({body: {url: "https://zonehaven.com"}})
+        .then ((result) => {
+            expect(result).to.be.a("object");
+            expect(result).to.has.property("body");
+            let obj = JSON.parse(result.body);
+            expect(obj).to.has.property("data");
+            expect(obj.data).to.has.property("title");
+            expect(obj.data).to.has.property("description");
+            done();
+        }).catch(done);
+    });
+
+    it("should return the bad request error", (done) => {
+        // const getStub = Sinon.stub(request, 'get').resolves({url: "", body: ``})
+        urlMetadataScrapper({body: {data: "https://zonehaven.com"}})
+        .then ((result) => {
+            expect(result).to.be.a("object");
+            expect(result).to.has.property("body");
+            let obj = JSON.parse(result.body);
+            expect(obj).to.has.property("message");
+            expect(obj.message).eqls("Bad Request!")
+            done();
+        }).catch(done);
+    });
+
+    it("should handle the failed cases", (done) => {
+        // const getStub = Sinon.stub(request, 'get').resolves({url: "", body: ``})
+        urlMetadataScrapper({body: {url: "https://zonehsfefaven.com"}})
+        .then ((result) => {
+            expect(result).to.be.a("object");
+            expect(result).to.has.property("body");
+            let obj = JSON.parse(result.body);
+            expect(obj).to.has.property("message");
+            expect(obj.message).eqls("Can't retrieve data!")
+            done();
+        }).catch(done);
+    });
+});
